d4eb0c819dcb0c2d36effac3648dd83428bd2cc7
Anirban <geekay0184@gmail.com>
2024-03-09 18:33:58 +0600

    nothing updates

--- newsProject/admin/delete-user.php
+++ newsProject/admin/delete-user.php
@@ -6,5 +6,5 @@ $sql = "DELETE FROM user WHERE id = '{$userId}' ";
 $result = mysqli_query($conn, $sql);
 
 header("Location: http://localhost/mysqlPhp/newsProject/admin/users.php");
-mysqli_close($conn);
+mysqli_close($conn);   
 ?>
