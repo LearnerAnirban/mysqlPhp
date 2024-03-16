a964ebc629aa7cbf145f4cda057da4411441b70d
Anirban <geekay0184@gmail.com>
2024-03-16 15:55:26 +0600

    news font end

--- newsProject/admin/delete-post.php
+++ newsProject/admin/delete-post.php
@@ -1,9 +1,23 @@
 <?php
   include "config.php";
-  $post_id = $_GET['id'];
-  $sql = "DELETE FROM post WHERE post_id = '{$post_id}'";
-  $result = mysqli_query($conn, $sql) or die("query failed");
+  $post_id = mysqli_real_escape_string($conn, $_GET['id']);
+  $cat_id = mysqli_real_escape_string($conn, $_GET['catid']);
 
-  header("Location: {$hostname}/admin/post.php");
-  mysqli_close($result);
+  $sql1 = "SELECT * FROM post WHERE post_id = '{$post_id}'";
+  $result = mysqli_query($conn, $sql1) or die("Query Failed");
+  $row = mysqli_fetch_assoc($result);
+  //delete the image from folder
+  unlink("upload/" . $row['post_img']);
+
+  $sql = "DELETE FROM post WHERE post_id = '{$post_id}';";
+  $sql .= "UPDATE category SET post = post - 1 WHERE category_id = {$cat_id}";
+  if(mysqli_multi_query($conn, $sql)) {
+    mysqli_close($conn);
+    header("Location: {$hostname}/admin/post.php");
+
+  } else {
+    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+  }
+
+
 ?>
